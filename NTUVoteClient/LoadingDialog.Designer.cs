7511747239855cdd8b9b01f75ec6d87f10eb4006
Poren Chiang <ren.chiang@gmail.com>
2018-05-31 03:11:06 +0800

    Add nicer messages and loading dialog

---
+++ NTUVoteClient/LoadingDialog.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace NTUOSC.Vote
+{
+    partial class LoadingDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.image = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // image
+            // 
+            this.image.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.image.Location = new System.Drawing.Point(12, 12);
+            this.image.Name = "image";
+            this.image.Size = new System.Drawing.Size(168, 168);
+            this.image.TabIndex = 0;
+            this.image.TabStop = false;
+            // 
+            // LoadingDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(192, 192);
+            this.ControlBox = false;
+            this.Controls.Add(this.image);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "LoadingDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            ((System.ComponentModel.ISupportInitialize)(this.image)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox image;
+    }
+}
---
+++ NTUVoteClient/LoadingDialog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace NTUOSC.Vote
+{
+    public partial class LoadingDialog : Form
+    {
+        public LoadingDialog()
+        {
+            InitializeComponent();
+        }
+
+        public void SetImage(string name)
+        {
+            image.ImageLocation = Path.GetFullPath(String.Format("Resources\\{0}.gif", name));
+        }
+
+        public void ShowCentered(Form parent)
+        {
+            Show(parent);
+            Location = new Point(parent.Location.X + (parent.Width - Width) / 2, parent.Location.Y + (parent.Height - Height) / 2);
+        }
+    }
+}
